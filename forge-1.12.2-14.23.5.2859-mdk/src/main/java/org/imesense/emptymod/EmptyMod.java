5463432ddefa6277dd937cfb195c7f9fd1b44b6a
acidicMercury8 <acidicmercury8@outlook.com>
2024-09-10 21:12:42 +0300

    Add documenting comments
    Co-authored-by: OldSerpskiStalker <ictopiar@gmail.com>

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/org/imesense/emptymod/EmptyMod.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/org/imesense/emptymod/EmptyMod.java
@@ -1,12 +1,17 @@
 package org.imesense.emptymod;
 
 import net.minecraft.init.Blocks;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
 import org.apache.logging.log4j.Logger;
 
+/**
+ * Main class of modification
+ */
 @Mod(
     modid = EmptyMod.MODID,
     name = EmptyMod.NAME,
@@ -14,18 +19,42 @@ import org.apache.logging.log4j.Logger;
 )
 public class EmptyMod
 {
+    /**
+     * Modification ID
+     */
     public static final String MODID = "emptymod";
+
+    /**
+     * Modification name
+     */
     public static final String NAME = "Empty Mod";
+
+    /**
+     * Minecraft version
+     */
     public static final String VERSION = "1.12.2-14.23.5.2859";
 
+    /**
+     * Logger object
+     */
     private static Logger logger;
 
+    /**
+     * Preinitialize modification
+     * 
+     * @param event Preinitialization event
+     */
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
     }
 
+    /**
+     * Initialize modification
+     * 
+     * @param event Initialization event
+     */
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
