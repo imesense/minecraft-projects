0d99924ed79b91d623df0391d685cb6b36a6998d
acidicMercury8 <acidicmercury8@outlook.com>
2024-09-12 12:03:34 +0300

    Add missing proxy classes
    Co-authored-by: OldSerpskiStalker <ictopiar@gmail.com>

--- forge-1.12.2-14.23.5.2859-mdk/src/main/java/org/imesense/emptymod/EmptyMod.java
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/org/imesense/emptymod/EmptyMod.java
@@ -4,10 +4,17 @@ import net.minecraft.init.Blocks;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.event.FMLServerStoppedEvent;
 
 import org.apache.logging.log4j.Logger;
+
+import org.imesense.emptymod.proxy.IProxy;
 
 /**
  * Main class of modification
@@ -40,14 +47,31 @@ public class EmptyMod
     private static Logger logger;
 
     /**
+     * Main class instance
+     */
+    @Mod.Instance
+    public static EmptyMod Instance;
+
+    /**
+     * Sided proxy settings
+     */
+    @SidedProxy(
+        clientSide = "org.imesense.emptymod.proxy.ClientProxy",
+        serverSide = "org.imesense.emptymod.proxy.ServerProxy"
+    )
+    public static IProxy Proxy;
+
+    /**
      * Preinitialize modification
      * 
      * @param event Preinitialization event
      */
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    public synchronized void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
+
+        Proxy.preInit(event);
     }
 
     /**
@@ -56,8 +80,51 @@ public class EmptyMod
      * @param event Initialization event
      */
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public synchronized void init(FMLInitializationEvent event)
     {
+        Proxy.init(event);
+
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    /**
+     * Postinitialize modification
+     * 
+     * @param event Postinitialization event
+     */
+    @EventHandler
+    public synchronized void postInit(FMLPostInitializationEvent event)
+    {
+        Proxy.postInit(event);
+    }
+
+    /**
+     * Load complete action
+     * 
+     * @param event Load complete event
+     */
+    @EventHandler
+    public synchronized void onLoadComplete(FMLLoadCompleteEvent event)
+    {
+    }
+
+    /**
+     * Server load action
+     * 
+     * @param event Server starting event
+     */
+    @EventHandler
+    public synchronized void serverLoad(FMLServerStartingEvent event)
+    {
+    }
+
+    /**
+     * Server stopped action
+     * 
+     * @param event Server stopped action
+     */
+    @EventHandler
+    public synchronized void serverStopped(FMLServerStoppedEvent event)
+    {
     }
 }
---
+++ forge-1.12.2-14.23.5.2859-mdk/src/main/java/org/imesense/emptymod/proxy/IProxy.java
@@ -0,0 +1,69 @@
+package org.imesense.emptymod.proxy;
+
+import java.util.concurrent.Callable;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Sided proxy
+ */
+public interface IProxy
+{
+    /**
+     * Preinitialize modification
+     * 
+     * @param event Preinitialization event
+     */
+    void preInit(FMLPreInitializationEvent event);
+
+    /**
+     * Initialize modification
+     * 
+     * @param event Initialization event
+     */
+    void init(FMLInitializationEvent event);
+
+    /**
+     * Postinitialize modification
+     * 
+     * @param event Postinitialization event
+     */
+    void postInit(FMLPostInitializationEvent event);
+
+    /**
+     * Get client world instance
+     * 
+     * @return Current world instance
+     */
+    World getClientWorld();
+
+    /**
+     * Get client player instance
+     * 
+     * @return Current player instance
+     */
+    EntityPlayer getClientPlayer();
+
+    /**
+     * Add task to schedule for execution on client
+     * 
+     * @param <T> Type of task result
+     * @param callableToSchedule Task to complete
+     * @return Task result
+     */
+    <T> ListenableFuture<T> addScheduledTaskClient(Callable<T> callableToSchedule);
+
+    /**
+     * Add task to schedule for execution on client
+     * 
+     * @param runnableToSchedule Task to complete
+     * @return Task result
+     */
+    ListenableFuture<Object> addScheduledTaskClient(Runnable runnableToSchedule);
+}
