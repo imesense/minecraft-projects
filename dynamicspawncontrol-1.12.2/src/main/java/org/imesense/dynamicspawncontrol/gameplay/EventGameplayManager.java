6daafcd48a148b26850e1fa860cca23341056147
OldSerpskiStalker <ictopiar@gmail.com>
2024-09-17 20:18:22 +0500

    Add reflection to initialize gameplay handlers

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/gameplay/EventGameplayManager.java
@@ -0,0 +1,34 @@
+package org.imesense.dynamicspawncontrol.gameplay;
+
+import net.minecraftforge.common.MinecraftForge;
+import org.imesense.dynamicspawncontrol.gameplay.events.OnUpdateTorchLogic;
+import org.imesense.dynamicspawncontrol.technical.customlibrary.Log;
+
+public final class EventGameplayManager
+{
+    private static final Class<?>[] EVENT_CLASSES =
+    {
+        OnUpdateTorchLogic.class
+    };
+
+    public EventGameplayManager()
+    {
+
+    }
+
+    public static void registerClasses()
+    {
+        for (Class<?> eventClass : EVENT_CLASSES)
+        {
+            try
+            {
+                Object eventInstance = eventClass.getConstructor(String.class).newInstance(eventClass.getSimpleName());
+                MinecraftForge.EVENT_BUS.register(eventInstance);
+            }
+            catch (Exception exception)
+            {
+                Log.writeDataToLogFile(Log.TypeLog[2], "Exception in class: " + eventClass.getName() + " - " + exception.getMessage());
+            }
+        }
+    }
+}
