9538a77a0124d167b9b6ba04a11d2fbe92c7067d
OldSerpskiStalker <ictopiar@gmail.com>
2024-09-18 23:53:08 +0500

    Add abstract classes for eventprocessor

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/eventprocessor/SignalDataAccessor.java
@@ -0,0 +1,78 @@
+package org.imesense.dynamicspawncontrol.technical.eventprocessor;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ *
+ * @param <ClassEvent>
+ */
+public abstract class SignalDataAccessor<ClassEvent>
+{
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract int getY(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract World getWorld(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract BlockPos getPos(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract Entity getEntity(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract ItemStack getItem(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract Entity getAttacker(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract EntityPlayer getPlayer(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract DamageSource getSource(ClassEvent classEvent);
+
+    /**
+     *
+     * @param classEvent
+     * @return
+     */
+    public abstract BlockPos getValidBlockPos(ClassEvent classEvent);
+}
---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/eventprocessor/SignalDataGetter.java
@@ -0,0 +1,43 @@
+package org.imesense.dynamicspawncontrol.technical.eventprocessor;
+
+import net.minecraft.world.World;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ *
+ */
+public abstract class SignalDataGetter
+{
+    /**
+     *
+     * @return
+     */
+    public abstract World getWorld();
+
+    /**
+     *
+     * @return
+     */
+    public abstract Entity getEntity();
+
+    /**
+     *
+     * @return
+     */
+    public abstract BlockPos getPosition();
+
+    /**
+     *
+     * @return
+     */
+    public abstract EntityPlayer getPlayer();
+
+    /**
+     *
+     * @return
+     */
+    public abstract EntityLivingBase getEntityLiving();
+}
