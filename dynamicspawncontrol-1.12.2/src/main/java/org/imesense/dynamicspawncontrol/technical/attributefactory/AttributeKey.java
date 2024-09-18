b01568a84c08e809aea72f5b2eb429dc81217408
OldSerpskiStalker <ictopiar@gmail.com>
2024-09-18 12:58:51 +0500

    Implementation of attribute maps (Deprecated)

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/attributefactory/AttributeKey.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/attributefactory/AttributeKey.java
@@ -1,6 +1,42 @@
 package org.imesense.dynamicspawncontrol.technical.attributefactory;
 
-public class AttributeKey
-{
+import javax.annotation.Nonnull;
 
+public final class AttributeKey<T>
+{
+    private final String name;
+
+    private final AttributeType<T> type;
+
+    public AttributeKey(@Nonnull AttributeType<T> type,
+                        @Nonnull String name)
+    {
+        this.type = type;
+        this.name = name;
+    }
+
+    @Nonnull
+    public static <T> AttributeKey<T> create(@Nonnull AttributeType<T> type,
+                                             @Nonnull String code)
+    {
+        return new AttributeKey<>(type, code);
+    }
+
+    @Nonnull
+    public AttributeType<T> getType()
+    {
+        return this.type;
+    }
+
+    @Nonnull
+    public String getName()
+    {
+        return this.name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Key(" + this.name + ')';
+    }
 }
--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/attributefactory/AttributeType.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/attributefactory/AttributeType.java
@@ -1,6 +1,126 @@
 package org.imesense.dynamicspawncontrol.technical.attributefactory;
 
-public class AttributeType
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ *
+ * @param <T>
+ */
+public final class AttributeType<T>
 {
-    
+    /**
+     *
+     */
+    public static final AttributeType<Long> LONG = create(Long.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<Float> FLOAT = create(Float.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<String> JSON = create(String.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<Double> DOUBLE = create(Double.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<String> STRING = create(String.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<Boolean> BOOLEAN = create(Boolean.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<Integer> INTEGER = create(Integer.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<?> MAP = create(AttributeMap.class);
+
+    /**
+     *
+     */
+    public static final AttributeType<Object> OBJECT = new AttributeType<>(Object.class);
+
+    /**
+     *
+     */
+    private final Class<T> type;
+
+    /**
+     *
+     * @param type
+     */
+    private AttributeType(@Nonnull Class<T> type)
+    {
+        this.type = type;
+    }
+
+    /**
+     *
+     * @param type
+     * @return
+     * @param <T>
+     */
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    public static <T> AttributeType<T> create(@Nonnull Class<? super T> type)
+    {
+        return new AttributeType<>((Class<T>) type);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Nonnull
+    public Class<T> getType()
+    {
+        return this.type;
+    }
+
+    /**
+     *
+     * @param list
+     * @return
+     */
+    @Nonnull
+    public List<T> convert(@Nonnull List<T> list)
+    {
+        return list;
+    }
+
+    /**
+     *
+     * @param javaObject
+     * @return
+     */
+    @Nonnull
+    @SuppressWarnings("unchecked")
+    public T convert(Object javaObject)
+    {
+        return (T) javaObject;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public String toString()
+    {
+        return "Type(" + getType().getSimpleName() + ')';
+    }
 }
