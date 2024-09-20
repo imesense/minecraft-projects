f007cfc0dacb99ad2012cdffc145b1423ce7fc60
OldSerpskiStalker <ictopiar@gmail.com>
2024-09-20 12:12:52 +0500

    Add new generic in CodeGenericUtils

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/debug/CodeGenericUtils.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/debug/CodeGenericUtils.java
@@ -1,5 +1,7 @@
 package org.imesense.dynamicspawncontrol.debug;
 
+import org.imesense.dynamicspawncontrol.technical.attributefactory.AttributeKey;
+import org.imesense.dynamicspawncontrol.technical.attributefactory.AttributeMap;
 import org.imesense.dynamicspawncontrol.technical.customlibrary.Log;
 
 /**
@@ -23,5 +25,46 @@ public class CodeGenericUtils
         }
 
         return object;
+    }
+
+    /**
+     *
+     * @param mobMap
+     * @param parameterName
+     * @param min
+     * @param max
+     * @param logParameterName
+     * @return
+     * @param <T>
+     */
+    public static <T extends Number> T checkParameter(AttributeMap<?> mobMap, AttributeKey<?> parameterName, T min, T max, String logParameterName)
+    {
+        Object value = mobMap.get(parameterName);
+
+        if (value == null)
+        {
+            Log.writeDataToLogFile(Log.TypeLog[2], "Parameter '" + logParameterName + "' missing or return null");
+            throw new RuntimeException();
+        }
+
+        T numericValue;
+
+        try
+        {
+            numericValue = (T) value;
+        }
+        catch (ClassCastException e)
+        {
+            Log.writeDataToLogFile(Log.TypeLog[2], "Parameter '" + logParameterName + "' has an invalid type");
+            throw new RuntimeException();
+        }
+
+        if (numericValue.doubleValue() < min.doubleValue() || numericValue.doubleValue() > max.doubleValue())
+        {
+            Log.writeDataToLogFile(Log.TypeLog[2], "An error was detected in the parameter '" + logParameterName + "': range [" + min + " .. " + max + "]");
+            throw new RuntimeException();
+        }
+
+        return numericValue;
     }
 }
