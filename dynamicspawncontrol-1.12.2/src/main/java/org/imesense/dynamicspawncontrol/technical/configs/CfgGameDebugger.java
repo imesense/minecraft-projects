5dc725096c3bb6114d865d65400bd7d1366ca2fc
OldSerpskiStalker <ictopiar@gmail.com>
2024-09-28 16:20:56 +0500

    Implementation of an config abstract class (Part 1)

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/configs/CfgGameDebugger.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/configs/CfgGameDebugger.java
@@ -3,6 +3,7 @@ package org.imesense.dynamicspawncontrol.technical.configs;
 import com.google.gson.*;
 import com.google.gson.annotations.JsonAdapter;
 import org.imesense.dynamicspawncontrol.debug.CodeGenericUtils;
+import org.imesense.dynamicspawncontrol.technical.configs.cfgGameDebugger.DataCategories;
 import org.imesense.dynamicspawncontrol.technical.customlibrary.Log;
 
 import javax.annotation.Nonnull;
@@ -19,59 +20,14 @@ public final class CfgGameDebugger extends CustomConceptConfig
 {
     public static CfgGameDebugger instance;
 
-    private final DebugMonitor monitor;
-    private final DebugEvent event;
-
-    public static final class DebugMonitor
-    {
-        final String setCategory;
-
-        private static boolean monitorDebug = false;
-
-        public DebugMonitor(@Nonnull final String category)
-        {
-            this.setCategory = category;
-        }
-
-        public static Boolean getDebugMonitorOpt()
-        {
-            return monitorDebug;
-        }
-
-        public static void setDebugMonitorOpt(Boolean monitorDebug_)
-        {
-            monitorDebug = monitorDebug_;
-        }
-    }
-
-    public static final class DebugEvent
-    {
-        final String setCategory;
-        private boolean drop_all_experience = false;
-
-        public DebugEvent(@Nonnull final String category)
-        {
-            this.setCategory = category;
-        }
-
-        public Boolean getDebugDropAllExpOpt()
-        {
-            return this.drop_all_experience;
-        }
-
-        public void setDebugDropAllExpOpt(Boolean drop_all_experience)
-        {
-            this.drop_all_experience = drop_all_experience;
-        }
-    }
-
     public CfgGameDebugger(String nameConfigFile) throws IOException
     {
         super(nameConfigFile);
 
         this.nameConfig = this.constructPathToDirectory() + nameConfigFile;
-        this.monitor = new DebugMonitor("monitor");
-        this.event = new DebugEvent("event");
+
+        DataCategories.DebugMonitor.instance = new DataCategories.DebugMonitor("monitor");
+        DataCategories.DebugEvent.instance = new DataCategories.DebugEvent("event");
 
         if (Files.exists(Paths.get(this.nameConfig)))
         {
@@ -88,13 +44,14 @@ public final class CfgGameDebugger extends CustomConceptConfig
     public void saveToFile() throws IOException
     {
         JsonObject jsonObject = new JsonObject();
-
         JsonObject monitorObject = new JsonObject();
-        monitorObject.addProperty("monitorDebug", monitor.getDebugMonitorOpt());
+
+        monitorObject.addProperty("monitorDebug", DataCategories.DebugMonitor.instance.getDebugMonitorOpt());
         jsonObject.add("monitor", monitorObject);
 
         JsonObject eventObject = new JsonObject();
-        eventObject.addProperty("drop_all_exp", event.getDebugDropAllExpOpt());
+
+        eventObject.addProperty("drop_all_exp", DataCategories.DebugEvent.instance.getDebugDropAllExpOpt());
         jsonObject.add("event", eventObject);
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -107,33 +64,20 @@ public final class CfgGameDebugger extends CustomConceptConfig
 
     public void loadFromFile() throws IOException
     {
-        try (FileReader reader = new FileReader(this.nameConfig))
+        FileReader reader = new FileReader(this.nameConfig);
+        JsonElement jsonElement = new JsonParser().parse(reader);
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+        if (jsonObject.has("monitor"))
         {
-            JsonElement jsonElement = new JsonParser().parse(reader);
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-            if (jsonObject.has("monitor"))
-            {
-                JsonObject monitorObject = jsonObject.getAsJsonObject("monitor");
-                this.monitor.setDebugMonitorOpt(monitorObject.get("monitorDebug").getAsBoolean());
-            }
-
-            if (jsonObject.has("event"))
-            {
-                JsonObject eventObject = jsonObject.getAsJsonObject("event");
-                this.event.setDebugDropAllExpOpt(eventObject.get("drop_all_exp").getAsBoolean());
-            }
+            JsonObject monitorObject = jsonObject.getAsJsonObject("monitor");
+            DataCategories.DebugMonitor.instance.setDebugMonitorOpt(monitorObject.get("monitorDebug").getAsBoolean());
         }
-    }
 
-    // Геттеры для доступа к внутренним объектам
-    public DebugMonitor getMonitor()
-    {
-        return monitor;
-    }
-
-    public DebugEvent getEvent()
-    {
-        return event;
+        if (jsonObject.has("event"))
+        {
+            JsonObject eventObject = jsonObject.getAsJsonObject("event");
+            DataCategories.DebugEvent.instance.setDebugDropAllExpOpt(eventObject.get("drop_all_exp").getAsBoolean());
+        }
     }
 }
--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/configs/cfgGameDebugger/DataCategories.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/configs/cfgGameDebugger/DataCategories.java
@@ -1,6 +1,54 @@
 package org.imesense.dynamicspawncontrol.technical.configs.cfgGameDebugger;
 
+import javax.annotation.Nonnull;
+
 public class DataCategories
 {
+    public static final class DebugMonitor
+    {
+        final String setCategory;
 
+        public static DebugMonitor instance;
+
+        private boolean monitorDebug = false;
+
+        public DebugMonitor(@Nonnull final String category)
+        {
+            this.setCategory = category;
+        }
+
+        public Boolean getDebugMonitorOpt()
+        {
+            return monitorDebug;
+        }
+
+        public void setDebugMonitorOpt(Boolean monitorDebug)
+        {
+            this.monitorDebug = monitorDebug;
+        }
+    }
+
+    public static final class DebugEvent
+    {
+        final String setCategory;
+
+        public static DebugEvent instance;
+
+        private boolean drop_all_experience = false;
+
+        public DebugEvent(@Nonnull final String category)
+        {
+            this.setCategory = category;
+        }
+
+        public Boolean getDebugDropAllExpOpt()
+        {
+            return this.drop_all_experience;
+        }
+
+        public void setDebugDropAllExpOpt(Boolean drop_all_experience)
+        {
+            this.drop_all_experience = drop_all_experience;
+        }
+    }
 }
