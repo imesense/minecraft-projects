cf01f76fadc3add749a2abf080a7890f8a69a995
OldSerpskiStalker <ictopiar@gmail.com>
2024-10-04 23:47:20 +0500

    The concept of complexity of biomes (continued, test)

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/debug/events/OnEventDummy.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/debug/events/OnEventDummy.java
@@ -2,6 +2,7 @@ package org.imesense.dynamicspawncontrol.debug.events;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -11,6 +12,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.imesense.dynamicspawncontrol.debug.CodeGenericUtils;
 import org.imesense.dynamicspawncontrol.technical.customlibrary.Log;
 
+import static net.minecraft.client.gui.Gui.drawModalRectWithCustomSizedTexture;
 import static net.minecraft.client.gui.Gui.drawRect;
 
 /**
@@ -78,10 +80,10 @@ public final class OnEventDummy
     @SubscribeEvent
     public void onRenderOverlay(RenderGameOverlayEvent.Text event) {
         long currentTime = System.currentTimeMillis();
-        
+
         if (currentTime - lastBiomeChangeTime < 5000) {
-            int boxWidth = 200;
-            int boxHeight = 50;
+            int boxWidth = 125;
+            int boxHeight = 80; // Увеличим высоту, чтобы влезла текстура (50 + 128)
 
             int xPos = 0;
             int yPos = 0;
@@ -94,6 +96,21 @@ public final class OnEventDummy
             int textYPos = yPos + 10;
 
             mc.fontRenderer.drawString(biomeText, textXPos, textYPos, 0xFFFFFF);
+
+            // Отрисовка текстуры PNG после текста
+            ResourceLocation texture = new ResourceLocation("dynamicspawncontrol", "textures/gui/hardbiom.png");
+            mc.getTextureManager().bindTexture(texture);
+
+            int textureXPos = textXPos; // текстура будет на том же уровне по X
+            int textureYPos = textYPos + 20; // текстура будет ниже текста на 20 пикселей
+
+            int textureWidth = 128 / 7;
+            int textureHeight = 128 / 7;
+
+            // Отрисовка текстуры (Текстура размером 128x128 пикселей)
+            drawModalRectWithCustomSizedTexture(textureXPos, textureYPos, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
+            drawModalRectWithCustomSizedTexture(textureXPos + 20, textureYPos, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
+            drawModalRectWithCustomSizedTexture(textureXPos + 40, textureYPos, 0, 0, textureWidth, textureHeight, textureWidth, textureHeight);
         }
     }
 }
