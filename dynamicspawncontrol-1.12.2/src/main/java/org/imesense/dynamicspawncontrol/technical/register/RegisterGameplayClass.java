34064152ed1609f951ca7b076a9c5838355127be
OldSerpskiStalker <ictopiar@gmail.com>
2024-10-28 13:13:06 +0500

    Add new class 'OnNickNameZombie'

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/register/RegisterGameplayClass.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/technical/register/RegisterGameplayClass.java
@@ -4,10 +4,7 @@ import net.minecraftforge.common.MinecraftForge;
 import org.imesense.dynamicspawncontrol.debug.CodeGenericUtil;
 import org.imesense.dynamicspawncontrol.gameplay.event.OnUpdateTorchLogic;
 import org.imesense.dynamicspawncontrol.technical.customlibrary.Log;
-import org.imesense.dynamicspawncontrol.technical.eventprocessor.primitive.OnComplexityBiomes;
-import org.imesense.dynamicspawncontrol.technical.eventprocessor.primitive.OnDropHeadMob;
-import org.imesense.dynamicspawncontrol.technical.eventprocessor.primitive.OnDropSkeletonItem;
-import org.imesense.dynamicspawncontrol.technical.eventprocessor.primitive.OnDropZombieItem;
+import org.imesense.dynamicspawncontrol.technical.eventprocessor.primitive.*;
 
 /**
  *
@@ -23,7 +20,8 @@ public final class RegisterGameplayClass
         OnDropZombieItem.class,
         OnDropSkeletonItem.class,
         OnComplexityBiomes.class,
-        OnDropHeadMob.class
+        OnDropHeadMob.class,
+        OnNickNameZombie.class
     };
 
     /**
