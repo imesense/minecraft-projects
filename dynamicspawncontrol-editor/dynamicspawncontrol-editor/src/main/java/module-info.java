9f7a0e41f76ff30e1f10be9abcac57db55fc71aa
OldSerpskiStalker <ictopiar@gmail.com>
2024-11-05 20:32:54 +0500

    Fix start

--- dynamicspawncontrol-editor/dynamicspawncontrol-editor/src/main/java/module-info.java
+++ dynamicspawncontrol-editor/dynamicspawncontrol-editor/src/main/java/module-info.java
@@ -8,7 +8,6 @@ module org.imesense.dynamicspawncontroleditor.dynamicspawncontroleditor {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
     opens org.imesense.dynamicspawncontroleditor.dynamicspawncontroleditor to javafx.fxml;
