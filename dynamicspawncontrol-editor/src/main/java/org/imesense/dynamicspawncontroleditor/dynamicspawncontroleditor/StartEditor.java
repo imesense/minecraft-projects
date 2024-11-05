8ec4c32f609d056826425513959df0908c051527
OldSerpskiStalker <ictopiar@gmail.com>
2024-11-05 22:12:55 +0500

    Add CSS Style for background-color

--- dynamicspawncontrol-editor/src/main/java/org/imesense/dynamicspawncontroleditor/dynamicspawncontroleditor/StartEditor.java
+++ dynamicspawncontrol-editor/src/main/java/org/imesense/dynamicspawncontroleditor/dynamicspawncontroleditor/StartEditor.java
@@ -9,6 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.imesense.dynamicspawncontroleditor.dynamicspawncontroleditor.core.logfile.LogFile;
 
 import java.io.IOException;
@@ -37,18 +38,24 @@ public class StartEditor extends Application
     public void start(Stage primaryStage) throws IOException
     {
         String logPath = System.getProperty("user.dir");
-        boolean IDEA_RT = true; //System.getProperty("java.class.path").toLowerCase().contains("idea_rt.jar");
+        boolean IDEA_RT = true;//System.getProperty("java.class.path").toLowerCase().contains("idea_rt.jar");
 
         LogFile.createLogFile(logPath, IDEA_RT);
 
         Parent mainWindowApp = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainWindowApp.fxml")));
 
-        primaryStage.setTitle("Dynamic Spawn Control Editor");
-
         Scene scene = new Scene(mainWindowApp);
 
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
+
+        primaryStage.setTitle("Dynamic Spawn Control Editor");
+
+        primaryStage.setWidth(1280);
+        primaryStage.setHeight(720);
+
+        primaryStage.setResizable(false);
+
         primaryStage.setScene(scene);
-        primaryStage.setFullScreen(false);
         primaryStage.show();
 
         LogFile.writeDataToLogFile(0, "Редактор запущен");
