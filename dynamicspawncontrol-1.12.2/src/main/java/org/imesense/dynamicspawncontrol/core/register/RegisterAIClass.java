309f5e37fc9776dd3c20bdda9dab7e6c9839976e
OldSerpskiStalker <ictopiar@gmail.com>
2024-12-09 09:14:16 +0500

    A separate implementation class for initializing AI classes

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/register/RegisterAIClass.java
@@ -0,0 +1,54 @@
+package org.imesense.dynamicspawncontrol.core.register;
+
+import net.minecraftforge.common.MinecraftForge;
+import org.imesense.dynamicspawncontrol.ai.spider.event.OnEventAvoidLight;
+import org.imesense.dynamicspawncontrol.ai.zombie.event.OnEventBreakTorch;
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+import org.imesense.dynamicspawncontrol.plugin.webslinger_1_12_2_2_2_4.capability.EventHandler;
+
+/**
+ * 
+ */
+public final class RegisterAIClass
+{
+    /**
+     *
+     */
+    private static final Class<?>[] EVENT_CLASSES =
+    {
+        EventHandler.class,
+        OnEventBreakTorch.class,
+        OnEventAvoidLight.class
+    };
+
+    /**
+     *
+     */
+    public RegisterAIClass()
+    {
+        CodeGeneric.printInitClassToLog(this.getClass());
+    }
+
+    /**
+     *
+     */
+    public static void registerClasses()
+    {
+        for (Class<?> _class : EVENT_CLASSES)
+        {
+            try
+            {
+                Object object =
+                        _class.getConstructor().newInstance();
+
+                MinecraftForge.EVENT_BUS.register(object);
+            }
+            catch (Exception exception)
+            {
+                Log.writeDataToLogFile(2, "Exception in class: " + _class.getName() + " - " + exception.getMessage());
+                throw new RuntimeException(exception);
+            }
+        }
+    }
+}
