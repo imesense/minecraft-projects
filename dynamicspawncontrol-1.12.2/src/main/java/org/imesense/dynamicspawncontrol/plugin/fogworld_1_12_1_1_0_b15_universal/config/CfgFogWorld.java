95cf39ec7f1f47a719fb5785ee41c47ed65c7723
OldSerpskiStalker <ictopiar@gmail.com>
2025-02-16 17:58:36 +0500

    Porting the plugin fogworld_1_12_1_1_0_b15_universal (Part 1)
    Регулятор тумана в мире, для погоды и других событий

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/plugin/fogworld_1_12_1_1_0_b15_universal/config/CfgFogWorld.java
@@ -0,0 +1,25 @@
+package org.imesense.dynamicspawncontrol.plugin.fogworld_1_12_1_1_0_b15_universal.config;
+
+import org.imesense.dynamicspawncontrol.core.annotation.ConceptConfig;
+import org.imesense.dynamicspawncontrol.core.api.AConfig;
+
+@ConceptConfig(fileName = "cfg_fog_world_1_12_1_1_0_b15_universal")
+public class CfgFogWorld extends AConfig
+{
+    public CfgFogWorld(String nameConfigFile)
+    {
+        super(nameConfigFile, Boolean.FALSE);
+    }
+
+    @Override
+    public void saveToFile()
+    {
+
+    }
+
+    @Override
+    public void loadFromFile()
+    {
+
+    }
+}
---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/plugin/fogworld_1_12_1_1_0_b15_universal/proxy/ClientProxy.java
@@ -0,0 +1,4 @@
+package org.imesense.dynamicspawncontrol.plugin.fogworld_1_12_1_1_0_b15_universal.proxy;
+
+public class ClientProxy extends CommonProxy {
+}
