d8cfdf71752daf9ef399894abaaa09c40fc1ccf9
OldSerpskiStalker <ictopiar@gmail.com>
2025-02-27 23:36:20 +0500

    Solid and volatile constants with a singleton pattern at the initialization stage

--- src/main/java/org/imesense/dynamicspawncontrol/core/script/actioncollector/Priority.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/actioncollector/Priority.java
@@ -1,4 +1,47 @@
 package org.imesense.dynamicspawncontrol.core.script.actioncollector;
 
-public class Priority {
+import org.imesense.dynamicspawncontrol.core.script.storage.StoringScriptData;
+
+import java.util.List;
+import java.util.Random;
+
+public final class Priority
+{
+    private static volatile Priority _INSTANCE;
+
+    public static Priority getInstance()
+    {
+        if (_INSTANCE == null)
+        {
+            synchronized (Priority.class)
+            {
+                if (_INSTANCE == null)
+                {
+                    _INSTANCE = new Priority();
+                }
+            }
+        }
+
+        return _INSTANCE;
+    }
+
+    public StoringScriptData.Equipment getConfigByPriority(List<StoringScriptData.Equipment> equipmentList, Random random)
+    {
+        int totalPriority = equipmentList.stream().mapToInt(config -> config.Priority).sum();
+        int randomValue = random.nextInt(totalPriority);
+
+        int cumulativePriority = 0;
+
+        for (StoringScriptData.Equipment config : equipmentList)
+        {
+            cumulativePriority += config.Priority;
+
+            if (randomValue < cumulativePriority)
+            {
+                return config;
+            }
+        }
+
+        return equipmentList.get(equipmentList.size() - 1);
+    }
 }
---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/initializer/ConceptScriptProcessor.java
@@ -0,0 +1,28 @@
+package org.imesense.dynamicspawncontrol.core.script.initializer;
+
+import org.imesense.dynamicspawncontrol.core.debug.Timer;
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.script.actioncollector.*;
+
+public class ConceptScriptProcessor
+{
+    public ConceptScriptProcessor()
+    {
+        Timer timer = new Timer();
+
+        timer.start();
+        Equip equip = Equip.getInstance();
+        double equipTime = timer.stop();
+        Log.writeDataToLogFile(0, "Create Action Collector object 'Equip': " + equip.hashCode() + " (Time: " + equipTime + " ms)");
+
+        timer.start();
+        Potion potion = Potion.getInstance();
+        double potionTime = timer.stop();
+        Log.writeDataToLogFile(0, "Create Action Collector object 'Potion': " + potion.hashCode() + " (Time: " + potionTime + " ms)");
+
+        timer.start();
+        Priority priority = Priority.getInstance();
+        double priorityTime = timer.stop();
+        Log.writeDataToLogFile(0, "Create Action Collector object 'Priority': " + priority.hashCode() + " (Time: " + priorityTime + " ms)");
+    }
+}
