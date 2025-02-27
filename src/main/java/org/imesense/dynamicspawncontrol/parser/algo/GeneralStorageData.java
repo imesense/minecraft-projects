f121a0e0afc8c0291636f71348a57391df9510c1
OldSerpskiStalker <ictopiar@gmail.com>
2025-02-27 12:24:53 +0500

    A new parser for working with entities

--- src/main/java/org/imesense/dynamicspawncontrol/parser/algo/GeneralStorageData.java
+++ src/main/java/org/imesense/dynamicspawncontrol/parser/algo/GeneralStorageData.java
@@ -1,95 +1,56 @@
 package org.imesense.dynamicspawncontrol.parser.algo;
 
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
 
 import java.util.List;
 
-/**
- *
- */
 public final class GeneralStorageData
 {
-    /**
-     *
-     */
     public static GeneralStorageData Instance;
 
-    /**
-     *
-     */
     public GeneralStorageData()
     {
-		CodeGeneric.printInitClassToLog(this.getClass());
-
+        CodeGeneric.printInitClassToLog(this.getClass());
         Instance = this;
     }
 
-    /**
-     *
-     */
-    public List<String> EntitiesProhibitedOutdoors;
-
-    /**
-     *
-     * @return
-     */
-    public List<String> getEntitiesProhibitedOutdoors()
-    {
-        return this.EntitiesProhibitedOutdoors;
-    }
-
-    /**
-     *
-     */
     public static class Equipment
     {
-        /**
-         *
-         */
+        public String entityType;
         public int Priority;
 
-        /**
-         *
-         */
         public List<String> HeldItems;
-
-        /**
-         *
-         */
         public List<String> Helmets;
-
-        /**
-         *
-         */
         public List<String> ChestPlates;
-
-        /**
-         *
-         */
         public List<String> Leggings;
-
-        /**
-         *
-         */
         public List<String> Boots;
-
-        /**
-         *
-         */
         public boolean HasShield;
     }
 
-    /**
-     *
-     */
     public List<Equipment> EquipmentConfigs;
+    public List<PotionEffectWithChance> Potions;
 
-    /**
-     *
-     * @return
-     */
     public List<Equipment> getEquipmentConfigs()
     {
         return this.EquipmentConfigs;
+    }
+
+    public List<PotionEffectWithChance> getPotions()
+    {
+        return this.Potions;
+    }
+
+    public static class PotionEffectWithChance
+    {
+        public double Chance;
+        public PotionEffect Effect;
+
+        public PotionEffectWithChance(PotionEffect effect, double chance)
+        {
+            this.Effect = effect;
+            this.Chance = chance;
+        }
     }
 }
