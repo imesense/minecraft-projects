66d0788e4e195e5c9f845e4d0c2438f5d2b2f40a
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-07 00:55:04 +0500

    Porting the old syntax for potential spawn

--- src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventPotentialSpawn.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventPotentialSpawn.java
@@ -6,7 +6,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
+import org.imesense.dynamicspawncontrol.core.field.UniqueField;
 import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.script.storage.potentialspawn.data.SecondaryParameters;
 import org.imesense.dynamicspawncontrol.core.script.storage.potentialspawn.storage.GeneralPotentialSpawnStorage;
 
 import java.util.List;
@@ -17,16 +19,26 @@ public final class OnEventPotentialSpawn
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void onPotentialSpawns(WorldEvent.PotentialSpawns event)
     {
-        List<Biome.SpawnListEntry> spawnEntries = GeneralPotentialSpawnStorage.getInstance().spawnEntries;
+        GeneralPotentialSpawnStorage storage = GeneralPotentialSpawnStorage.getInstance();
+        List<Biome.SpawnListEntry> spawnEntries = storage.spawnEntries;
+        List<SecondaryParameters.Data> secondaryParameters = storage.secondaryParameters;
 
-        if (spawnEntries.isEmpty())
+        if (spawnEntries.isEmpty() || secondaryParameters.isEmpty())
         {
             return;
         }
 
-        for (Biome.SpawnListEntry entry : spawnEntries)
+        int eventY = event.getPos().getY();
+
+        for (int i = 0; i < spawnEntries.size(); i++)
         {
-            event.getList().add(entry);
+            Biome.SpawnListEntry entry = spawnEntries.get(i);
+            SecondaryParameters.Data data = secondaryParameters.get(i);
+
+            if (UniqueField.RANDOM.nextFloat() < data.spawnChance && eventY >= data.minHeight && eventY <= data.maxHeight)
+            {
+                event.getList().add(entry);
+            }
         }
     }
 }
