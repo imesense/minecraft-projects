3663fb3d573126bec7b38bbd3a521a3709d771ce
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-06 11:38:43 +0500

    Removed empty comments from the code

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/worldcache/CacheEntityStorage.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/worldcache/CacheEntityStorage.java
@@ -5,19 +5,10 @@ import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
 
 import java.util.List;
 
-/**
- *
- */
 public final class CacheEntityStorage
 {
-    /**
-     *
-     */
     public static CacheEntityStorage Instance;
 
-    /**
-     *
-     */
     public CacheEntityStorage()
     {
 		CodeGeneric.printInitClassToLog(this.getClass());
@@ -25,25 +16,13 @@ public final class CacheEntityStorage
         Instance = this;
     }
 
-    /**
-     *
-     */
     public List<EntityData> EntityCacheMobs;
 
-    /**
-     *
-     * @return
-     */
     public List<EntityData> getEntityCacheMobs()
     {
         return this.EntityCacheMobs;
     }
 
-    /**
-     *
-     * @param resourceLocation
-     * @return
-     */
     public CacheEntityStorage.EntityData getEntityDataByResourceLocation(ResourceLocation resourceLocation)
     {
         for (CacheEntityStorage.EntityData entityData : getEntityCacheMobs())
@@ -57,45 +36,23 @@ public final class CacheEntityStorage
         return null;
     }
 
-    /**
-     *
-     */
     public static class EntityData
     {
-        /**
-         *
-         */
         private final int MAX_COUNT;
 
-        /**
-         *
-         */
         private final ResourceLocation RESOURCE_LOCATION;
 
-        /**
-         *
-         * @param entity
-         * @param maxCount
-         */
         public EntityData(ResourceLocation entity, int maxCount)
         {
             this.RESOURCE_LOCATION = entity;
             this.MAX_COUNT = maxCount;
         }
 
-        /**
-         *
-         * @return
-         */
         public ResourceLocation getEntity()
         {
             return this.RESOURCE_LOCATION;
         }
 
-        /**
-         *
-         * @return
-         */
         public int getMaxCount()
         {
             return this.MAX_COUNT;
