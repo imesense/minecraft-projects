6df5e5d8b46c5249d77d323d0f02c10a44cb4b40
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-07 19:14:03 +0500

    Realization MobTaskManager - "enemies_to" <-> "to_them". Part 1

--- src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventMobTaskManager.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventMobTaskManager.java
@@ -1,11 +1,19 @@
 package org.imesense.dynamicspawncontrol.core.script.parser;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.core.api.AbstractConceptParser;
 import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.script.storage.mobtaskmanager.storage.GeneralMobTaskManager;
 import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ParserEventMobTaskManager extends AbstractConceptParser
 {
@@ -18,8 +26,8 @@ public class ParserEventMobTaskManager extends AbstractConceptParser
     @Override
     public void loadConfig(boolean init)
     {
-        File file = getConfigFile(init,
-                DynamicSpawnControlStructure.STRUCT_FILES_DIRS.NAME_DIR_GAME_SCRIPTS, this.nameFile);
+        File file =
+                getConfigFile(init, DynamicSpawnControlStructure.STRUCT_FILES_DIRS.NAME_DIR_GAME_SCRIPTS, this.nameFile);
 
         if (!file.exists())
         {
@@ -27,6 +35,45 @@ public class ParserEventMobTaskManager extends AbstractConceptParser
             createNewConfigFile(file);
             return;
         }
+
+        try (FileReader fileReader = new FileReader(file))
+        {
+            JsonParser parser = new JsonParser();
+            JsonArray jsonArray = parser.parse(fileReader).getAsJsonArray();
+
+            GeneralMobTaskManager taskManager = GeneralMobTaskManager.getInstance();
+
+            for (JsonElement topLevelElement : jsonArray)
+            {
+                JsonObject topLevelObject = topLevelElement.getAsJsonObject();
+
+                GeneralMobTaskManager.EntityHostilityToThem hostility = new GeneralMobTaskManager.EntityHostilityToThem();
+                hostility.enemies_to = parseStringArray(topLevelObject.getAsJsonArray("enemies_to"));
+                hostility.to_them = parseStringArray(topLevelObject.getAsJsonArray("to_them"));
+
+                taskManager.addEnemy(hostility);
+            }
+        }
+        catch (FileNotFoundException exception)
+        {
+            throw new RuntimeException(exception);
+        }
+        catch (IOException exception)
+        {
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private String[] parseStringArray(JsonArray jsonArray)
+    {
+        String[] array = new String[jsonArray.size()];
+
+        for (int i = 0; i < jsonArray.size(); i++)
+        {
+            array[i] = jsonArray.get(i).getAsString();
+        }
+
+        return array;
     }
 
     @Override
---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/storage/mobtaskmanager/data/EntityHostility.java
@@ -0,0 +1,6 @@
+package org.imesense.dynamicspawncontrol.core.script.storage.mobtaskmanager.data;
+
+public class EntityHostility
+{
+    
+}
