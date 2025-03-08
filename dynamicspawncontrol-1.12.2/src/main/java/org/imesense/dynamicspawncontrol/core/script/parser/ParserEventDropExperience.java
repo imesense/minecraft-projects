86eaebc825385f48dbcc752e006beec3aecc171a
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-08 18:34:00 +0500

    Implementation OnEventDropExperience

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventDropExperience.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventDropExperience.java
@@ -1,5 +1,12 @@
 package org.imesense.dynamicspawncontrol.core.script.parser;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.EntityEntry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.core.api.AbstractConceptParser;
 import org.imesense.dynamicspawncontrol.core.logfile.Log;
@@ -7,6 +14,9 @@ import org.imesense.dynamicspawncontrol.core.script.storage.dropexperience.stora
 import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.BiFunction;
 
 public final class ParserEventDropExperience extends AbstractConceptParser
 {
@@ -14,6 +24,23 @@ public final class ParserEventDropExperience extends AbstractConceptParser
     {
         CodeGeneric.printInitClassToLog(this.getClass());
         this.nameFile = NAME_FILE;
+    }
+
+    public static <T> T getValueFromJson(JsonObject jsonObject, String key, T defaultValue, BiFunction<JsonElement, T, T> biFunction)
+    {
+        Log.writeDataToLogFile(0, "Read jsonObject: " + jsonObject);
+
+        if (jsonObject.has(key))
+        {
+            JsonElement jsonElement = jsonObject.get(key);
+
+            if (jsonElement.isJsonPrimitive() && jsonElement.getAsJsonPrimitive().isNumber())
+            {
+                return biFunction.apply(jsonElement, defaultValue);
+            }
+        }
+
+        return defaultValue;
     }
 
     @Override
@@ -27,6 +54,47 @@ public final class ParserEventDropExperience extends AbstractConceptParser
             Log.writeDataToLogFile(0, "Config file not found, creating new: " + file);
             createNewConfigFile(file);
             return;
+        }
+
+        try (FileReader reader = new FileReader(file))
+        {
+            JsonParser parser = new JsonParser();
+            JsonArray jsonArray = parser.parse(reader).getAsJsonArray();
+
+            for (JsonElement element : jsonArray)
+            {
+                JsonObject jsonObject = element.getAsJsonObject();
+                GeneralDropExperience.Data data = new GeneralDropExperience.Data();
+
+                String entityId = jsonObject.get("entity").getAsString();
+                data.entity = entityId;
+
+                EntityEntry ee = ForgeRegistries.ENTITIES.getValue(new ResourceLocation(entityId));
+
+                if (ee == null)
+                {
+                    Log.writeDataToLogFile(0, "Entity not found: " + entityId);
+                    continue;
+                }
+
+                data.xp = getValueFromJson(jsonObject,
+                        "xp", 0, (el, defaultValue) -> el.getAsInt());
+
+                data.multi_xp = getValueFromJson(jsonObject,
+                        "multi_xp", 0.f, (el, defaultValue) -> el.getAsFloat());
+
+                data.adding_xp = getValueFromJson(jsonObject,
+                        "adding_xp", 0.f, (el, defaultValue) -> el.getAsFloat());
+
+                Log.writeDataToLogFile(0, "Loaded data for entity: " + data.entity + ", xp: " + data.xp + ", multi_xp: " + data.multi_xp + ", adding_xp: " + data.adding_xp);
+
+                GeneralDropExperience.getInstance().dropExperienceList.add(data);
+            }
+        }
+        catch (IOException exception)
+        {
+            Log.writeDataToLogFile(0, "Failed to load config file: " + file);
+            exception.printStackTrace();
         }
     }
 
--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/storage/dropexperience/storage/GeneralDropExperience.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/storage/dropexperience/storage/GeneralDropExperience.java
@@ -33,6 +33,7 @@ public final class GeneralDropExperience
 
     public static final class Data
     {
+        public String entity;
         public Integer xp;
         public Float multi_xp;
         public Float adding_xp;
