6241ecdb29711f786247cdcc3c3423e5e4dbdcc4
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-08 19:01:10 +0500

    Replacing the cycle and function with a filter

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventDropExperience.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventDropExperience.java
@@ -7,10 +7,7 @@ import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
-import org.imesense.dynamicspawncontrol.core.logfile.Log;
 import org.imesense.dynamicspawncontrol.core.script.storage.dropexperience.storage.GeneralDropExperience;
-
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = DynamicSpawnControlStructure.STRUCT_INFO_MOD.MOD_ID)
 public final class OnEventDropExperience
@@ -19,7 +16,6 @@ public final class OnEventDropExperience
     public void onUpdateLivingExperienceDrop_0(LivingExperienceDropEvent livingExperienceDropEvent)
     {
         Entity entity = livingExperienceDropEvent.getEntity();
-
         ResourceLocation entityResourceLocation = EntityList.getKey(entity);
 
         if (entityResourceLocation == null)
@@ -27,31 +23,15 @@ public final class OnEventDropExperience
             return;
         }
 
-        List<GeneralDropExperience.Data> dropExperienceList = GeneralDropExperience.getInstance().dropExperienceList;
+        GeneralDropExperience.getInstance().dropExperienceList.stream()
+                .filter(data -> entityResourceLocation.equals(data.entity))
+                .findFirst()
+                .ifPresent(data ->
+                {
+                    Integer originalXp = livingExperienceDropEvent.getDroppedExperience();
+                    Integer modifyXp = (int) ((data.xp != 0 ? data.xp : originalXp) * data.multi_xp + data.adding_xp);
 
-        for (GeneralDropExperience.Data data : dropExperienceList)
-        {
-            if (entityResourceLocation.equals(data.entity))
-            {
-                int originalXp = livingExperienceDropEvent.getDroppedExperience();
-                int modifyXp = modifyXp(originalXp, data);
-
-                livingExperienceDropEvent.setDroppedExperience(modifyXp);
-
-                return;
-            }
-        }
-    }
-
-    private int modifyXp(int xpIn, GeneralDropExperience.Data data)
-    {
-        if (data.xp != 0)
-        {
-            xpIn = data.xp;
-        }
-
-        int modifiedXp = (int) (xpIn * data.multi_xp + data.adding_xp);
-
-        return modifiedXp;
+                    livingExperienceDropEvent.setDroppedExperience(modifyXp);
+                });
     }
 }
