270c129c6e6525d53ce3888fb47e749ae15c6c7e
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-08 20:50:24 +0500

    Implementation OnEventDropItem

--- src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventDropItem.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/parser/ParserEventDropItem.java
@@ -1,11 +1,20 @@
 package org.imesense.dynamicspawncontrol.core.script.parser;
 
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import net.minecraft.util.ResourceLocation;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.core.api.AbstractConceptParser;
 import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.script.storage.dropitem.storage.GeneralDropItem;
 import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParserEventDropItem extends AbstractConceptParser
 {
@@ -27,11 +36,46 @@ public class ParserEventDropItem extends AbstractConceptParser
             createNewConfigFile(file);
             return;
         }
+
+        try (FileReader reader = new FileReader(file))
+        {
+            JsonParser parser = new JsonParser();
+            JsonArray jsonArray = parser.parse(reader).getAsJsonArray();
+
+            List<GeneralDropItem.Data> dataList = new ArrayList<>();
+
+            for (JsonElement element : jsonArray)
+            {
+                JsonObject jsonObject = element.getAsJsonObject();
+
+                GeneralDropItem.Data data = new GeneralDropItem.Data();
+                data.entity = new ResourceLocation(jsonObject.get("entity").getAsString());
+
+                JsonArray dropsArray = jsonObject.getAsJsonArray("drop");
+                data.drops = new ArrayList<>();
+
+                for (int i = 0; i < dropsArray.size(); i += 2)
+                {
+                    GeneralDropItem.Data.ItemDrop itemDrop = new GeneralDropItem.Data.ItemDrop();
+                    itemDrop.item = new ResourceLocation(dropsArray.get(i).getAsString());
+                    itemDrop.amount = dropsArray.get(i + 1).getAsInt();
+                    data.drops.add(itemDrop);
+                }
+
+                dataList.add(data);
+            }
+
+            GeneralDropItem.getInstance().dropItemList.addAll(dataList);
+        }
+        catch (IOException exception)
+        {
+            Log.writeDataToLogFile(0, "Failed to load drop item config: " + exception.getMessage());
+        }
     }
 
     @Override
     public void eraseData()
     {
-
+        GeneralDropItem.getInstance().dropItemList.clear();
     }
 }
