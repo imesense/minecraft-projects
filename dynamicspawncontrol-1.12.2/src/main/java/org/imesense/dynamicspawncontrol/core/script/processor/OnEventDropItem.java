d2d0cc39ed7b544772d813d5e3d664f5bd79a7dd
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-08 20:15:57 +0500

    Add empty classes for DropItem parser

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventDropItem.java
@@ -0,0 +1,13 @@
+package org.imesense.dynamicspawncontrol.core.script.processor;
+
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class OnEventDropItem
+{
+    @SubscribeEvent
+    public void onUpdateLivingDrops_0(LivingDropsEvent livingDropsEvent)
+    {
+
+    }
+}
