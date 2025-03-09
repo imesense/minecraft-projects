03e7e11aec32da4f794ebedec7aacd318b23de81
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 20:55:48 +0500

    New event system. Part 7

--- src/main/java/org/imesense/dynamicspawncontrol/core/api/BaseParserRegister.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/api/BaseParserRegister.java
@@ -1,6 +1,56 @@
 package org.imesense.dynamicspawncontrol.core.api;
 
+import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class BaseParserRegister
 {
+    protected abstract Class<?>[] getParserClasses();
 
+    private final List<BaseParser> PARSER_LIST = new ArrayList<>();
+
+    public BaseParserRegister()
+    {
+        CodeGeneric.printInitClassToLog(this.getClass());
+    }
+
+    public void init()
+    {
+        for (Class<?> parserClass : getParserClasses())
+        {
+            try
+            {
+                BaseParser parser = (BaseParser) parserClass.getConstructor(String.class)
+                        .newInstance(getParserName(parserClass));
+
+                PARSER_LIST.add(parser);
+
+                parser.loadConfig(true);
+            }
+            catch (Exception exception)
+            {
+                Log.writeDataToLogFile(2, "Exception initializing parser: " + parserClass.getName() + " - " + exception.getMessage());
+                throw new RuntimeException(exception);
+            }
+        }
+    }
+
+    public void reloadAllConfigs()
+    {
+        for (BaseParser parser : PARSER_LIST)
+        {
+            parser.reloadConfig();
+        }
+    }
+
+    private String getParserName(Class<?> parserClass)
+    {
+        return parserClass.getSimpleName()
+                .replaceAll("([a-z])([A-Z]+)", "$1_$2")
+                .toLowerCase() + DynamicSpawnControlStructure.STRUCT_FILES_EXTENSION.SCRIPT_FILE_EXTENSION;
+    }
 }
--- src/main/java/org/imesense/dynamicspawncontrol/core/register/parser/RegisterParser.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/register/parser/RegisterParser.java
@@ -1,8 +1,43 @@
 package org.imesense.dynamicspawncontrol.core.register.parser;
 
 import org.imesense.dynamicspawncontrol.core.api.BaseParserRegister;
+import org.imesense.dynamicspawncontrol.core.register.config.RegisterConfig;
+import org.imesense.dynamicspawncontrol.core.script.parser.*;
 
-public class RegisterParser extends BaseParserRegister
+public final class RegisterParser extends BaseParserRegister
 {
+    private static volatile RegisterParser _INSTANCE;
 
+    public static RegisterParser getInstance()
+    {
+        if (_INSTANCE == null)
+        {
+            synchronized (RegisterParser.class)
+            {
+                if (_INSTANCE == null)
+                {
+                    _INSTANCE = new RegisterParser();
+                }
+            }
+        }
+
+        return _INSTANCE;
+    }
+
+    private static final Class<?>[] PARSER_CLASSES =
+    {
+        ParserEventCacheSettings.class,
+        ParserEventCheckSpawn.class,
+        ParserEventDropExperience.class,
+        ParserEventDropItem.class,
+        ParserEventPotentialSpawn.class,
+        ParserEventPopulationChunk.class,
+        ParserEventMobTaskManager.class
+    };
+
+    @Override
+    protected Class<?>[] getParserClasses()
+    {
+        return PARSER_CLASSES;
+    }
 }
