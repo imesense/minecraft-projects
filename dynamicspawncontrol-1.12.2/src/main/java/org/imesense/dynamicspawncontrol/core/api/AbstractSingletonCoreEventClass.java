996bdff3c288e6098e414a1ce6a142e82983bfe2
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 13:08:50 +0500

    New event system. Part 2

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/api/AbstractSingletonCoreEventClass.java
@@ -0,0 +1,29 @@
+package org.imesense.dynamicspawncontrol.core.api;
+
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractSingletonCoreEventClass
+{
+    private static final Map<Class<?>, Boolean> INSTANCE_EXITS_MAP = new HashMap<>();
+
+    protected AbstractSingletonCoreEventClass()
+    {
+        Class<?> clazz = this.getClass();
+        CodeGeneric.printInitClassToLog(clazz);
+
+        synchronized (INSTANCE_EXITS_MAP)
+        {
+            if (INSTANCE_EXITS_MAP.getOrDefault(clazz, false))
+            {
+                Log.writeDataToLogFile(2, String.format("An instance of [%s] already exists!", clazz.getSimpleName()));
+                throw new RuntimeException();
+            }
+
+            INSTANCE_EXITS_MAP.put(clazz, true);
+        }
+    }
+}
--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/event/OnEventPopulateChunkEventPre.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/event/OnEventPopulateChunkEventPre.java
@@ -1,22 +1,8 @@
 package org.imesense.dynamicspawncontrol.core.event;
 
-import org.imesense.dynamicspawncontrol.core.logfile.Log;
-import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+import org.imesense.dynamicspawncontrol.core.api.AbstractSingletonCoreEventClass;
 
-public final class OnEventPopulateChunkEventPre
+public final class OnEventPopulateChunkEventPre extends AbstractSingletonCoreEventClass
 {
-    private static boolean instanceExists = false;
 
-    public OnEventPopulateChunkEventPre()
-    {
-        CodeGeneric.printInitClassToLog(this.getClass());
-
-        if (instanceExists)
-        {
-            Log.writeDataToLogFile(2, String.format("An instance of [%s] already exists!", this.getClass().getSimpleName()));
-            throw new RuntimeException();
-        }
-
-        instanceExists = true;
-    }
 }
