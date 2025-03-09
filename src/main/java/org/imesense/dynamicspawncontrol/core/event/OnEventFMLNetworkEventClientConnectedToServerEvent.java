4c1d7c02e7ec4d9053be4492d0605de324da37d6
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 12:55:16 +0500

    New event system. Part 1

---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/event/OnEventFMLNetworkEventClientConnectedToServerEvent.java
@@ -0,0 +1,22 @@
+package org.imesense.dynamicspawncontrol.core.event;
+
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+
+public final class OnEventFMLNetworkEventClientConnectedToServerEvent
+{
+    private static boolean instanceExists = false;
+
+    public OnEventFMLNetworkEventClientConnectedToServerEvent()
+    {
+        CodeGeneric.printInitClassToLog(this.getClass());
+
+        if (instanceExists)
+        {
+            Log.writeDataToLogFile(2, String.format("An instance of [%s] already exists!", this.getClass().getSimpleName()));
+            throw new RuntimeException();
+        }
+
+        instanceExists = true;
+    }
+}
---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/event/OnEventLivingSpawnEventSpecialSpawn.java
@@ -0,0 +1,22 @@
+package org.imesense.dynamicspawncontrol.core.event;
+
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+
+public final class OnEventLivingSpawnEventSpecialSpawn
+{
+    private static boolean instanceExists = false;
+
+    public OnEventLivingSpawnEventSpecialSpawn()
+    {
+        CodeGeneric.printInitClassToLog(this.getClass());
+
+        if (instanceExists)
+        {
+            Log.writeDataToLogFile(2, String.format("An instance of [%s] already exists!", this.getClass().getSimpleName()));
+            throw new RuntimeException();
+        }
+
+        instanceExists = true;
+    }
+}
