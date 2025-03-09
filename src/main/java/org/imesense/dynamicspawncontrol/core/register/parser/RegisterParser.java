5a41698daddd28d871698315a50842bd0374104c
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 18:36:54 +0500

    Add empty classes for BaseParserRegister

---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/register/parser/RegisterParser.java
@@ -0,0 +1,17 @@
+package org.imesense.dynamicspawncontrol.core.register.parser;
+
+import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
+import org.imesense.dynamicspawncontrol.core.api.AbstractConceptBaseConfigRegister;
+import org.imesense.dynamicspawncontrol.core.api.AbstractConceptBaseParserRegister;
+import org.imesense.dynamicspawncontrol.core.api.AbstractConceptParser;
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.register.config.RegisterConfig;
+import org.imesense.dynamicspawncontrol.core.script.parser.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisterParser extends AbstractConceptBaseParserRegister
+{
+
+}
