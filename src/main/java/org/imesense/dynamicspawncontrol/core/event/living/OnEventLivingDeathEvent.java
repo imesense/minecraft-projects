98cd7f8dbf4be9c4ad163031afdc61674bc7dee2
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 22:31:42 +0500

    Events are working correctly

--- src/main/java/org/imesense/dynamicspawncontrol/core/event/living/OnEventLivingDeathEvent.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/event/living/OnEventLivingDeathEvent.java
@@ -1,11 +1,19 @@
 package org.imesense.dynamicspawncontrol.core.event.living;
 
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.core.baseonevent.BaseOnEventInstance;
+import org.imesense.dynamicspawncontrol.eventdescriptions.DropHeadMob;
 
 @Mod.EventBusSubscriber(modid = DynamicSpawnControlStructure.STRUCT_INFO_MOD.MOD_ID)
 public final class OnEventLivingDeathEvent extends BaseOnEventInstance
 {
-
+    @SubscribeEvent(priority = EventPriority.LOW)
+    public void onLivingDeathEvent(LivingDeathEvent event)
+    {
+        DropHeadMob.getInstance().handleEntityDeath(event);
+    }
 }
