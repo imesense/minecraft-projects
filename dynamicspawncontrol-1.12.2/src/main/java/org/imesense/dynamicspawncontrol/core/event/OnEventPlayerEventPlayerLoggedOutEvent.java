e415ac0c421b3c9e43fdca6a7c967a8d56fd2cdd
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-09 12:55:16 +0500

    New event system. Part 1

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/event/OnEventPlayerEventPlayerLoggedOutEvent.java
@@ -0,0 +1,22 @@
+package org.imesense.dynamicspawncontrol.core.event;
+
+import org.imesense.dynamicspawncontrol.core.logfile.Log;
+import org.imesense.dynamicspawncontrol.core.util.CodeGeneric;
+
+public final class OnEventPlayerEventPlayerLoggedOutEvent
+{
+    private static boolean instanceExists = false;
+
+    public OnEventPlayerEventPlayerLoggedOutEvent()
+    {
+        CodeGeneric.printInitClassToLog(this.getClass());
+
+        if (instanceExists)
+        {
+            Log.writeDataToLogFile(2, String.format("An instance of [%s] already exists!", this.getClass().getSimpleName()));
+            throw new RuntimeException();
+        }
+
+        instanceExists = true;
+    }
+}
