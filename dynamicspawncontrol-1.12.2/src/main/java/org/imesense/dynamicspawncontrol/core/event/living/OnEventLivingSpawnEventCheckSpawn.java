4f4d5c8c7137ae9ae7c3b83d9fce41a97bc41c9a
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-14 21:02:59 +0500

    Implementation of caching for entities. Part 2

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/event/living/OnEventLivingSpawnEventCheckSpawn.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/event/living/OnEventLivingSpawnEventCheckSpawn.java
@@ -7,6 +7,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.core.baseonevent.BaseOnEventInstance;
 import org.imesense.dynamicspawncontrol.core.script.processor.OnEventCheckSpawn;
+import org.imesense.dynamicspawncontrol.core.script.processor.OnEventEntityWorldCache;
 
 @Mod.EventBusSubscriber(modid = DynamicSpawnControlStructure.STRUCT_INFO_MOD.MOD_ID)
 public final class OnEventLivingSpawnEventCheckSpawn extends BaseOnEventInstance
@@ -14,7 +15,7 @@ public final class OnEventLivingSpawnEventCheckSpawn extends BaseOnEventInstance
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void OnLivingSpawnEventCheckSpawnHIGHEST(LivingSpawnEvent.CheckSpawn event)
     {
-
+        OnEventEntityWorldCache.getInstance().handleLivingSpawnEventCheckSpawn(event);
     }
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
