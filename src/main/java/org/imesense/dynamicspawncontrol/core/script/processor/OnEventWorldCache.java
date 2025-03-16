4f6bd6d061833e9066a8688428ccd983655f41b9
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-16 19:19:20 +0500

    New functionality of the caching system. Part 3

--- src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventWorldCache.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/script/processor/OnEventWorldCache.java
@@ -2,14 +2,19 @@ package org.imesense.dynamicspawncontrol.core.script.processor;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.IAnimals;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.ClassInheritanceMultiMap;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.living.LivingSpawnEvent;
@@ -24,6 +29,8 @@ import org.imesense.dynamicspawncontrol.core.worldcache.CacheEntityStorage;
 import org.imesense.dynamicspawncontrol.core.worldcache.CacheMonitorDebug;
 
 import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 public final class OnEventWorldCache
 {
@@ -152,6 +159,133 @@ public final class OnEventWorldCache
 
     public void handleEntitySpawnEvent(LivingSpawnEvent.CheckSpawn event)
     {
-        
+        Entity entity = event.getEntity();
+        ResourceLocation entityKey = EntityList.getKey(entity);
+
+        if (entityKey == null)
+        {
+            return;
+        }
+
+        Optional<CacheEntityStorage.EntityData> optionalEntityData = CacheEntityStorage.getInstance()
+                .entityData.stream()
+                .filter(data -> data.entity.equals(entityKey))
+                .findFirst();
+
+        if (!optionalEntityData.isPresent())
+        {
+            return;
+        }
+
+        CacheEntityStorage.EntityData entityData = optionalEntityData.get();
+        WorldServer worldServer = (WorldServer) event.getWorld();
+        EntityPlayerMP nearestPlayer = getNearestPlayer(worldServer, event.getX(), event.getY(), event.getZ());
+
+        if (nearestPlayer == null)
+        {
+            return;
+        }
+
+        int currentEntityCount = getCurrentEntityCount(worldServer, nearestPlayer, entityKey);
+        int maxEntityCount = calculateMaxEntityCount(entityData, worldServer, nearestPlayer);
+
+        Log.writeDataToLogFile(0, "Entity: " + entityKey + ", Current Count: " + currentEntityCount + ", Max Count: " + maxEntityCount);
+
+        if (currentEntityCount >= maxEntityCount)
+        {
+            event.setResult(entityData.result);
+        }
+    }
+
+    private int calculateMaxEntityCount(CacheEntityStorage.EntityData entityData, WorldServer worldServer, EntityPlayerMP player)
+    {
+        int maxEntityCount = entityData.max_entity_count;
+
+        if (entityData.per_player)
+        {
+            int playerCount = getPlayerCount(worldServer);
+
+            if (playerCount > 1)
+            {
+                maxEntityCount = (int) (maxEntityCount * (0.5 + 0.5 * playerCount));
+            }
+        }
+
+        if (entityData.per_chunk)
+        {
+            int loadedChunkCount = getLoadedChunkCount(worldServer, player);
+            maxEntityCount = (int) (maxEntityCount * ((double) loadedChunkCount / 289));
+        }
+
+        return maxEntityCount;
+    }
+
+    private EntityPlayerMP getNearestPlayer(WorldServer worldServer, double x, double y, double z)
+    {
+        return (EntityPlayerMP) worldServer.getClosestPlayer(x, y, z, -1, false);
+    }
+
+    private int getLoadedChunkCount(WorldServer worldServer, EntityPlayerMP player)
+    {
+        Set<ChunkPos> validChunks = totalValidChunksSpawnForPlayer(worldServer, player);
+        return validChunks.size();
+    }
+
+    private int getPlayerCount(WorldServer worldServer)
+    {
+        return worldServer.getPlayers(EntityPlayerMP.class, player -> true).size();
+    }
+
+    private int getCurrentEntityCount(WorldServer worldServer, EntityPlayerMP player, ResourceLocation entityResource)
+    {
+        int count = 0;
+        Set<ChunkPos> validChunks = totalValidChunksSpawnForPlayer(worldServer, player);
+
+        for (ChunkPos chunkPos : validChunks)
+        {
+            Chunk chunk = worldServer.getChunkFromChunkCoords(chunkPos.x, chunkPos.z);
+
+            for (ClassInheritanceMultiMap<Entity> entityList : chunk.getEntityLists())
+            {
+                for (Entity entity : entityList)
+                {
+                    if (entity instanceof EntityLiving)
+                    {
+                        ResourceLocation entityLoc = EntityList.getKey(entity);
+
+                        if (entityLoc != null && entityLoc.equals(entityResource))
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private Set<ChunkPos> totalValidChunksSpawnForPlayer(WorldServer worldServer, EntityPlayerMP player)
+    {
+        Set<ChunkPos> validChunks = new HashSet<>();
+        int viewDistance = worldServer.getMinecraftServer().getPlayerList().getViewDistance();
+
+        int playerChunkX = MathHelper.floor(player.posX) >> 4;
+        int playerChunkZ = MathHelper.floor(player.posZ) >> 4;
+
+        for (int x = playerChunkX - viewDistance; x <= playerChunkX + viewDistance; x++)
+        {
+            for (int z = playerChunkZ - viewDistance; z <= playerChunkZ + viewDistance; z++)
+            {
+                ChunkPos chunkPos = new ChunkPos(x, z);
+
+                if (worldServer.getChunkProvider().isChunkGeneratedAt(x, z))
+                {
+                    validChunks.add(chunkPos);
+                }
+            }
+        }
+
+        return validChunks;
     }
 }
