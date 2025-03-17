7be9565aa7a1a0e10d083cd90bee1d9a573c7cf0
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-17 22:09:48 +0500

    An approximate implementation of the new concept of config files

---
+++ src/main/java/org/imesense/dynamicspawncontrol/core/config/DropItem/Data.java
@@ -0,0 +1,7 @@
+package org.imesense.dynamicspawncontrol.core.config.DropItem;
+
+@lombok.Data
+public class Data {
+   // private boolean test = true; // Дефолтное значение для test
+   // private int iTest = 42;      // Дефолтное значение для iTest
+}
