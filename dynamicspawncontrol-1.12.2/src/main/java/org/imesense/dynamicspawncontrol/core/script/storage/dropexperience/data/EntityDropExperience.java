3eabe42a9945cc7ff05e86cd8e4c4c618fb752d4
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-25 17:54:14 +0500

    Update ParserEventDropExperience. Support "use_default_xp", "time_min" and "time_max"

--- dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/storage/dropexperience/data/EntityDropExperience.java
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/script/storage/dropexperience/data/EntityDropExperience.java
@@ -1,6 +1,8 @@
 package org.imesense.dynamicspawncontrol.core.script.storage.dropexperience.data;
 
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.eventhandler.Event;
 
 public final class EntityDropExperience
 {
@@ -8,7 +10,33 @@ public final class EntityDropExperience
     {
         public ResourceLocation entity;
         public Integer xp;
-        public Float multi_xp;
+        public Float multi_xp = 1.0f;
         public Float adding_xp;
+        public Boolean use_default_xp = false;
+        public Long worldTimeIntervalMax;
+        public Long worldTimeIntervalMin;
+        public Event.Result result = Event.Result.DEFAULT;;
+
+        public boolean isTimeValid(World world)
+        {
+            if (this.worldTimeIntervalMin == null || this.worldTimeIntervalMax == null)
+            {
+                return true;
+            }
+
+            long worldTime = world.getWorldTime() % 24000;
+
+            return worldTime >= this.worldTimeIntervalMin && worldTime <= this.worldTimeIntervalMax;
+        }
+
+        public Boolean validate()
+        {
+            if (this.use_default_xp)
+            {
+                return this.multi_xp != null;
+            }
+
+            return true;
+        }
     }
 }
