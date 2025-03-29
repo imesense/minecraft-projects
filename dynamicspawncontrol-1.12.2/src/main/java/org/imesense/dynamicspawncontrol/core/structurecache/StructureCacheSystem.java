2b188cead1dc4b26960a5ee68c91d44a22439653
OldSerpskiStalker <ictopiar@gmail.com>
2025-03-29 22:21:01 +0500

    This is an inadequate solution, revert last commit

---
+++ dynamicspawncontrol-1.12.2/src/main/java/org/imesense/dynamicspawncontrol/core/structurecache/StructureCacheSystem.java
@@ -0,0 +1,19 @@
+package org.imesense.dynamicspawncontrol.core.structurecache;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public final class StructureCacheSystem
+{
+
+}
