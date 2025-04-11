23eb8d46d64b1343dc8d3f319edc43d57853cbbc
OldSerpskiStalker <ictopiar@gmail.com>
2025-04-12 01:50:04 +0500

    Add plugin 'spawnercraft' part 5 (fix launch game)

--- src/main/java/org/imesense/dynamicspawncontrol/core/plugin/mod/spawnercraft/bridge/NBTPreservingShapedRecipeWrapper.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/plugin/mod/spawnercraft/bridge/NBTPreservingShapedRecipeWrapper.java
@@ -3,8 +3,8 @@ package org.imesense.dynamicspawncontrol.core.plugin.mod.spawnercraft.bridge;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
-import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.BlankRecipeWrapper;
+//import mezz.jei.api.ingredients.IIngredients;
+//import mezz.jei.api.recipe.BlankRecipeWrapper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import org.imesense.dynamicspawncontrol.core.plugin.mod.spawnercraft.items.ItemMobSoul;
@@ -12,23 +12,23 @@ import org.imesense.dynamicspawncontrol.core.plugin.mod.spawnercraft.items.ItemM
 /* JADX INFO: Access modifiers changed from: package-private */
 /* compiled from: JEIBridge.java */
 /* loaded from: input.jar:cad97/spawnercraft/bridge/NBTPreservingShapedRecipeWrapper.class */
-public class NBTPreservingShapedRecipeWrapper extends BlankRecipeWrapper {
-    private final List<ItemStack> input;
-    private final ItemStack output;
+public class NBTPreservingShapedRecipeWrapper /*extends BlankRecipeWrapper*/ {
+    //private final List<ItemStack> input;
+    //private final ItemStack output;
 
     /* JADX INFO: Access modifiers changed from: package-private */
-    public NBTPreservingShapedRecipeWrapper(ResourceLocation variant, List<ItemStack> input, ItemStack output) {
-        this.input = (List) input.stream().map(itemStack -> {
-            ItemStack copy = itemStack.copy();
-            ItemMobSoul.applyEntityIdToItemStack(copy, variant);
-            return copy;
-        }).collect(Collectors.toList());
-        this.output = output.copy();
-        ItemMobSoul.applyEntityIdToItemStack(this.output, variant);
-    }
+    //public NBTPreservingShapedRecipeWrapper(ResourceLocation variant, List<ItemStack> input, ItemStack output) {
+  //      this.input = (List) input.stream().map(itemStack -> {
+   //         ItemStack copy = itemStack.copy();
+   //         ItemMobSoul.applyEntityIdToItemStack(copy, variant);
+   //         return copy;
+   //     }).collect(Collectors.toList());
+    //    this.output = output.copy();
+    //    ItemMobSoul.applyEntityIdToItemStack(this.output, variant);
+    //}
 
-    public void getIngredients(@Nonnull IIngredients ingredients) {
-        ingredients.setInputs(ItemStack.class, this.input);
-        ingredients.setOutput(ItemStack.class, this.output);
-    }
+    //public void getIngredients(@Nonnull IIngredients ingredients) {
+   //     ingredients.setInputs(ItemStack.class, this.input);
+   //     ingredients.setOutput(ItemStack.class, this.output);
+   // }
 }
