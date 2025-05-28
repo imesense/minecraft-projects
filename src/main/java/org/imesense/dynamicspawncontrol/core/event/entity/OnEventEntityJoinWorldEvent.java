5c1f14b934637d5984b2acacfe590582b3404546
OldSerpskiStalker <ictopiar@gmail.com>
2025-05-28 13:55:14 +0500

    Отключил тестовую логику для зомби, оставим на 0.2 и далее версии

--- src/main/java/org/imesense/dynamicspawncontrol/core/event/entity/OnEventEntityJoinWorldEvent.java
+++ src/main/java/org/imesense/dynamicspawncontrol/core/event/entity/OnEventEntityJoinWorldEvent.java
@@ -8,6 +8,7 @@ import org.imesense.dynamicspawncontrol.DynamicSpawnControlStructure;
 import org.imesense.dynamicspawncontrol.ai.spider.event.OnEventAvoidLight;
 import org.imesense.dynamicspawncontrol.ai.zombie.event.OnEventBreakTorch;
 import org.imesense.dynamicspawncontrol.core.annotation.InitLog;
+import org.imesense.dynamicspawncontrol.core.annotation.TODO;
 import org.imesense.dynamicspawncontrol.core.baseonevent.BaseOnEventInstance;
 import org.imesense.dynamicspawncontrol.core.memory.MemoryEvents;
 import org.imesense.dynamicspawncontrol.core.script.processor.OnEventMobTaskManager;
@@ -15,6 +16,9 @@ import org.imesense.dynamicspawncontrol.core.script.processor.OnEventWorldCache;
 import org.imesense.dynamicspawncontrol.eventdescriptions.PlayerNetwork;
 import org.imesense.dynamicspawncontrol.eventdescriptions.UpdateFire;
 
+@TODO(value = "Отключена до следующих версий, логика, когда зомби идет разбивать факел. Не правильно работает приоритет",
+        showOnce = false,
+        priority = TODO.TodoPriority.HIGH)
 @InitLog
 @Mod.EventBusSubscriber(modid = DynamicSpawnControlStructure.STRUCT_INFO_MOD.MOD_ID)
 public final class OnEventEntityJoinWorldEvent extends BaseOnEventInstance
@@ -24,6 +28,8 @@ public final class OnEventEntityJoinWorldEvent extends BaseOnEventInstance
 
     }
 
+    static boolean isTestLogic = false;
+
     @SubscribeEvent(priority = EventPriority.LOW)
     public void OnEntityJoinWorldEvent_LOW(EntityJoinWorldEvent event)
     {
@@ -31,7 +37,10 @@ public final class OnEventEntityJoinWorldEvent extends BaseOnEventInstance
 
         UpdateFire.getInstance().handleEntityJoinWorld(event);
 
-        OnEventBreakTorch.getInstance().handleSearchToBreakTorch(event);
+        if (isTestLogic)
+        {
+            OnEventBreakTorch.getInstance().handleSearchToBreakTorch(event);
+        }
 
         OnEventAvoidLight.getInstance().handleSpiderSpawn(event);
 
