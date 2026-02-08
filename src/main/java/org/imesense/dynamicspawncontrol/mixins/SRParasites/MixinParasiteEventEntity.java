7c100c15da19ebb45cd834989c2f119c1229aa97
OldSerpskiStalker <ictopiar@gmail.com>
2026-02-08 23:23:18 +0500

    Correction for modification 'SRParasites'

---
+++ src/main/java/org/imesense/dynamicspawncontrol/mixins/SRParasites/MixinParasiteEventEntity.java
@@ -0,0 +1,61 @@
+package org.imesense.dynamicspawncontrol.mixins.SRParasites;
+
+import com.dhanantry.scapeandrunparasites.util.ParasiteEventEntity;
+import org.imesense.dynamicspawncontrol.core.mixinconfig.MixinConfigManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(value = ParasiteEventEntity.class, remap = false)
+public class MixinParasiteEventEntity
+{
+    static
+    {
+        try
+        {
+            MixinConfigManager.loadConfig();
+        }
+        catch (Exception exception)
+        {
+            System.err.println("Failed to load mixin config: " + exception.getMessage());
+        }
+    }
+
+    @Inject(
+            method = "checkName",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private static void onCheckName(String potentialElement, String[] blacklist, boolean isWhitelist, CallbackInfoReturnable<Boolean> cir
+    )
+    {
+        if (!MixinConfigManager.isParasitesMixinEnabled())
+        {
+            return;
+        }
+
+        if (potentialElement == null)
+        {
+            return;
+        }
+
+        List<String> additionalBlacklist = MixinConfigManager.getParasitesBlacklist();
+
+        for (String blacklistedEntity : additionalBlacklist)
+        {
+            if (potentialElement.contains(blacklistedEntity))
+            {
+                if (!isWhitelist)
+                {
+                    cir.setReturnValue(true);
+                    cir.cancel();
+                }
+
+                return;
+            }
+        }
+    }
+}
