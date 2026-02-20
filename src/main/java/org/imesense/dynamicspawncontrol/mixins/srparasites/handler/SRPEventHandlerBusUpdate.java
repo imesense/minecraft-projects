98f19b0bb177b62f8c7942e791b4b9b1cab4843a
OldSerpskiStalker <ictopiar@gmail.com>
2026-02-20 14:38:41 +0500

    Delete log in SRPEventHandlerBus and ParasiteEventEntity

--- src/main/java/org/imesense/dynamicspawncontrol/mixins/srparasites/handler/SRPEventHandlerBusUpdate.java
+++ src/main/java/org/imesense/dynamicspawncontrol/mixins/srparasites/handler/SRPEventHandlerBusUpdate.java
@@ -6,8 +6,6 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.EntityList;
 import net.minecraft.nbt.NBTTagCompound;
 import org.imesense.dynamicspawncontrol.core.mixinconfig.srparasites.SRParasitesBlacklistData;
-import org.imesense.dynamicspawncontrol.core.logfile.EarlyLogBuffer;
-import org.imesense.dynamicspawncontrol.core.logfile.Log;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,45 +17,27 @@ public abstract class SRPEventHandlerBusUpdate
     @Inject(method = "writeCOTHTag", at = @At("HEAD"), remap = false)
     private void $writeCOTHTag(EntityLivingBase in, String mobname, NBTTagCompound tags, CallbackInfo ci)
     {
-        EarlyLogBuffer.log(Log.DEBUG, "SRPEventHandlerBusUpdate: writeCOTHTag called for mob: " + mobname);
-
         if (in != null)
         {
             ResourceLocation entityKey = EntityList.getKey(in);
-            EarlyLogBuffer.log(Log.DEBUG, "  Entity class: " + in.getClass().getName());
 
             if (entityKey != null)
             {
                 String entityName = entityKey.toString();
-                EarlyLogBuffer.log(Log.DEBUG, "  Entity registry name: " + entityName);
-
                 String[] blacklist = SRParasitesBlacklistData.getActiveBlacklist();
-                EarlyLogBuffer.log(Log.DEBUG, "  Blacklist size: " + (blacklist != null ? blacklist.length : 0));
 
                 if (blacklist != null)
                 {
                     for (String blacklistedEntity : blacklist)
                     {
-                        EarlyLogBuffer.log(Log.DEBUG, "    Checking against: " + blacklistedEntity);
-
                         if (entityName.equals(blacklistedEntity) || entityName.contains(blacklistedEntity))
                         {
-                            EarlyLogBuffer.log(Log.DEBUG, "  MATCH FOUND! Setting immunity to 0 for: " + entityName);
                             tags.setInteger("srpcothimmunity", 0);
-
                             break;
                         }
                     }
                 }
             }
-            else
-            {
-                EarlyLogBuffer.log(Log.DEBUG, "  Entity registry name is NULL");
-            }
-        }
-        else
-        {
-            EarlyLogBuffer.log(Log.DEBUG, "  Entity is NULL");
         }
     }
 }
--- src/main/java/org/imesense/dynamicspawncontrol/mixins/srparasites/util/ParasiteEventEntityUpdate.java
+++ src/main/java/org/imesense/dynamicspawncontrol/mixins/srparasites/util/ParasiteEventEntityUpdate.java
@@ -2,8 +2,6 @@ package org.imesense.dynamicspawncontrol.mixins.srparasites.util;
 
 import com.dhanantry.scapeandrunparasites.util.ParasiteEventEntity;
 import org.imesense.dynamicspawncontrol.core.mixinconfig.srparasites.SRParasitesBlacklistData;
-import org.imesense.dynamicspawncontrol.core.logfile.EarlyLogBuffer;
-import org.imesense.dynamicspawncontrol.core.logfile.Log;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -20,30 +18,19 @@ public abstract class ParasiteEventEntityUpdate
     )
     private static void $checkName(String potentialElement, String[] blacklist, boolean isWhitelist, CallbackInfoReturnable<Boolean> cir)
     {
-        EarlyLogBuffer.log(Log.DEBUG, "ParasiteEventEntityUpdate: checkName called");
-        EarlyLogBuffer.log(Log.DEBUG, "  potentialElement: " + potentialElement);
-        EarlyLogBuffer.log(Log.DEBUG, "  isWhitelist: " + isWhitelist);
-        EarlyLogBuffer.log(Log.DEBUG, "  original blacklist size: " + (blacklist != null ? blacklist.length : 0));
-
         if (potentialElement == null)
         {
-            EarlyLogBuffer.log(Log.DEBUG, "  potentialElement is NULL, returning");
             return;
         }
 
         String[] dscBlacklist = SRParasitesBlacklistData.getActiveBlacklist();
-        EarlyLogBuffer.log(Log.DEBUG, "  DSC blacklist size: " + (dscBlacklist != null ? dscBlacklist.length : 0));
 
         if (dscBlacklist != null)
         {
             for (String blacklistedEntity : dscBlacklist)
             {
-                EarlyLogBuffer.log(Log.DEBUG, "    Checking against: " + blacklistedEntity);
-
                 if (potentialElement.equals(blacklistedEntity) || potentialElement.contains(blacklistedEntity))
                 {
-                    EarlyLogBuffer.log(Log.DEBUG, "  MATCH FOUND! Setting return to: " + (!isWhitelist));
-
                     cir.setReturnValue(!isWhitelist);
                     cir.cancel();
 
@@ -51,7 +38,5 @@ public abstract class ParasiteEventEntityUpdate
                 }
             }
         }
-
-        EarlyLogBuffer.log(Log.DEBUG, "  No matches found, continuing to original method");
     }
 }
