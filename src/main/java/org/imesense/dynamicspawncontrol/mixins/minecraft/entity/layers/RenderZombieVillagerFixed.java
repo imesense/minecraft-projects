8a7d6a773d1533332a69d27e2f9090addf54f02a
OldSerpskiStalker <ictopiar@gmail.com>
2026-03-04 13:13:37 +0500

    Add RenderZombieVillagerFixed (later)

---
+++ src/main/java/org/imesense/dynamicspawncontrol/mixins/minecraft/entity/layers/RenderZombieVillagerFixed.java
@@ -0,0 +1,10 @@
+package org.imesense.dynamicspawncontrol.mixins.minecraft.entity.layers;
+
+import net.minecraft.client.renderer.entity.RenderZombieVillager;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(RenderZombieVillager.class)
+public abstract class RenderZombieVillagerFixed
+{
+
+}
