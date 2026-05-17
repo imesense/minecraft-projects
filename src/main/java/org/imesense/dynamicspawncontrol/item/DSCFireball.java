00028772beb1740fe8a2217b7d46602d55c3ae0d
OldSerpskiStalker <ictopiar@gmail.com>
2026-05-17 13:12:57 +0500

    The effect of smoldering earth near the explosion area for DSCFireball

--- src/main/java/org/imesense/dynamicspawncontrol/item/DSCFireball.java
+++ src/main/java/org/imesense/dynamicspawncontrol/item/DSCFireball.java
@@ -1,8 +1,10 @@
 package org.imesense.dynamicspawncontrol.item;
 
+import net.minecraft.entity.EntityAreaEffectCloud;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityFireball;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
@@ -47,7 +49,7 @@ public final class DSCFireball extends EntityFireball
     {
         if (!this.world.isRemote)
         {
-            this.world.createExplosion(this.shootingEntity,
+            this.world.createExplosion(null,
                     this.posX, this.posY, this.posZ, (float)this.explosionStrength, true);
 
             FireSpawnAction fireSpawnAction = (world, center, radius) ->
@@ -62,19 +64,18 @@ public final class DSCFireball extends EntityFireball
                     {
                         for (int z = -radius; z <= radius; z++)
                         {
-                            double distSq = x * x + y * y + z * z;
+                            double distSq = x*x + y*y + z*z;
 
-                            if (distSq > rSq)
-                                continue;
+                            if (distSq > rSq) continue;
 
-                            double noise = UniqueField.RANDOM.nextDouble() * radius * 0.8;
-
-                            if (distSq + noise > rSq)
-                                continue;
+                            if (Math.abs(Math.sqrt(distSq) - radius) > 1.5)
+                            {
+                                if (UniqueField.RANDOM.nextFloat() > 0.3f) continue;
+                            }
 
                             BlockPos pos = centerPos.add(x, y, z);
 
-                            if (isPositionVisibleFromCenter(world, centerPos, pos))
+                            if (world.isAirBlock(pos) && isPositionVisibleFromCenter(world, centerPos, pos))
                             {
                                 validFirePositions.add(pos);
                             }
@@ -84,7 +85,7 @@ public final class DSCFireball extends EntityFireball
 
                 for (BlockPos pos : validFirePositions)
                 {
-                    if (world.isAirBlock(pos) && UniqueField.RANDOM.nextFloat() < 0.12f)
+                    if (UniqueField.RANDOM.nextFloat() < 0.4f)
                     {
                         world.setBlockState(pos, Blocks.FIRE.getDefaultState());
                     }
@@ -92,8 +93,86 @@ public final class DSCFireball extends EntityFireball
             };
 
             fireSpawnAction.spawnFire(this.world, new BlockPos(this.posX, this.posY, this.posZ), 10);
+
+            spawnSurfaceSmoke(new BlockPos(this.posX, this.posY, this.posZ), 10);
+
             this.setDead();
         }
+    }
+
+    private void spawnSurfaceSmoke(BlockPos center, int radius)
+    {
+        int rSq = radius * radius;
+        List<BlockPos> smokePositions = new ArrayList<>();
+
+        for (int x = -radius; x <= radius; x++)
+        {
+            for (int z = -radius; z <= radius; z++)
+            {
+                int distSq = x*x + z*z;
+                if (distSq > rSq) continue;
+
+                double dist = Math.sqrt(distSq);
+                double edgeFactor = dist / radius;
+
+                if (UniqueField.RANDOM.nextFloat() > (0.15f + edgeFactor * 0.5f))
+                    continue;
+
+                for (int y = 4; y >= -4; y--)
+                {
+                    BlockPos pos = center.add(x, y, z);
+
+                    if (!world.isAirBlock(pos)) continue;
+
+                    BlockPos ground = pos.down();
+
+                    if (!world.isAirBlock(ground) &&
+                            world.getBlockState(ground).getMaterial().blocksMovement() &&
+                            world.getBlockState(ground).getBlock() != Blocks.FIRE &&
+                            world.getBlockState(ground).getBlock() != Blocks.LAVA &&
+                            world.getBlockState(ground).getBlock() != Blocks.FLOWING_LAVA)
+                    {
+                        smokePositions.add(pos);
+                        break;
+                    }
+                }
+            }
+        }
+
+        for (BlockPos pos : smokePositions)
+        {
+            if (UniqueField.RANDOM.nextFloat() < 0.65f)
+            {
+                spawnSmallSmokeCloud(pos);
+            }
+        }
+    }
+
+    private void spawnSmallSmokeCloud(BlockPos pos)
+    {
+        double x = pos.getX() + 0.5;
+        double y = pos.getY() + 0.1;
+        double z = pos.getZ() + 0.5;
+
+        EntityAreaEffectCloud cloud = new EntityAreaEffectCloud(world, x, y, z);
+
+        cloud.setRadius(1.0f + UniqueField.RANDOM.nextFloat() * 1.2f);
+        cloud.setRadiusPerTick(-0.0003f - UniqueField.RANDOM.nextFloat() * 0.0005f);
+
+        cloud.setDuration(1800 + UniqueField.RANDOM.nextInt(1200));
+        cloud.setWaitTime(0);
+
+        cloud.setParticle(EnumParticleTypes.SMOKE_LARGE);
+        cloud.setColor(0x3A3A3A);
+
+        cloud.motionX = 0;
+        cloud.motionZ = 0;
+        cloud.motionY = 0.015 + UniqueField.RANDOM.nextDouble() * 0.02;
+
+        cloud.setPosition(x + (UniqueField.RANDOM.nextDouble() - 0.5) * 0.3,
+                y, z + (UniqueField.RANDOM.nextDouble() - 0.5) * 0.3);
+
+        world.spawnEntity(cloud);
     }
 
     private boolean isPositionVisibleFromCenter(World world, BlockPos center, BlockPos target)
